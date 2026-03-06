5f6b6597d2679a3f72295bad14ce3891f1e3180f
Okan Durak <okandurakpro@gmail.com>
2026-03-06 13:23:29 +0300

    update

--- inc/woocommerce/hooks.php
+++ inc/woocommerce/hooks.php
@@ -1,7 +1,12 @@
 <?php
-if (!defined('ABSPATH')) exit;
+if (!defined('ABSPATH')) {
+    exit;
+}
 
-// Woo'nun "Sepeti görüntüle" linkli mesajını sadeleştir
-add_filter('wc_add_to_cart_message_html', function($message, $products){
-  return '<div class="woocommerce-message">Sepete eklendi.</div>';
+add_filter('wc_add_to_cart_message_html', function ($message, $products) {
+    return '<div class="woocommerce-message">Sepete eklendi.</div>';
 }, 10, 2);
+
+add_action('init', function () {
+    remove_action('woocommerce_before_checkout_form', 'woocommerce_checkout_coupon_form', 10);
+});
