d6de4d2c8397bd821e72bd64ea53b8549cf1d380
Okan Durak <okandurakpro@gmail.com>
2026-03-06 23:34:21 +0300

    update

--- inc/helpers.php
+++ inc/helpers.php
@@ -8,6 +8,7 @@ if (!defined('ABSPATH')) {
  */
 function polaris_get_login_page_url()
 {
+    // 1) PHP page template ile atanmis sayfa.
     $pages = get_posts([
         'post_type'      => 'page',
         'post_status'    => 'publish',
@@ -25,6 +26,18 @@ function polaris_get_login_page_url()
         }
     }
 
+    // 2) Siklikla kullanilan giris slug'lari.
+    $candidates = ['giris', 'login', 'hesaba-giris', 'hesabim-giris'];
+    foreach ($candidates as $slug) {
+        $page = get_page_by_path($slug, OBJECT, 'page');
+        if ($page instanceof WP_Post && $page->post_status === 'publish') {
+            $url = get_permalink((int) $page->ID);
+            if (!empty($url)) {
+                return $url;
+            }
+        }
+    }
+
     return '';
 }
 
@@ -36,10 +49,6 @@ function polaris_get_account_entry_url()
     $my_account_url = function_exists('wc_get_page_permalink')
         ? wc_get_page_permalink('myaccount')
         : wp_login_url();
-
-    if (is_user_logged_in()) {
-        return $my_account_url;
-    }
 
     $login_page_url = polaris_get_login_page_url();
     if (!empty($login_page_url)) {
